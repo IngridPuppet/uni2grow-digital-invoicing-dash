d59893c34b4a32b431443e2a272c7cb178d5c310
Ingrid Kamga <xingridkamga@gmail.com>
2023-06-30 12:13:06 +0100

    Fix warning about missing key

--- src/pages/invoices/PrintableInvoice.tsx
+++ src/pages/invoices/PrintableInvoice.tsx
@@ -74,14 +74,14 @@ export default function PrintableInvoice({invoice}: PrintableInvoiceProps) {
           </div>
 
           {
-            invoice.relInvoiceItems?.map((x) => <>
-              <div className="grid grid-cols-5 gap-4 px-4 py-2 even:bg-gray-200 text-xs app-inventory-item">
+            invoice.relInvoiceItems?.map((x) =>
+              <div key={x.id} className="grid grid-cols-5 gap-4 px-4 py-2 even:bg-gray-200 text-xs app-inventory-item">
                 <div className="col-span-2 font-semibold">{ x.item.name }</div>
                 <div className="col-span-1 text-right">{ x.quantity }</div>
                 <div className="col-span-1 text-right">${ x.priceOfRecord }</div>
                 <div className="col-span-1 text-right">${ handyMoney(x.quantity * x.priceOfRecord) }</div>
               </div>
-            </>)
+            )
           }
         </div>
 
